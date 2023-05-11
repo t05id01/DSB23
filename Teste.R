cfd3fe64f61c32fd0ebec959c5a5f7d8b1c0980b
t05id01 <gustmen@gmail.com>
2023-05-11 14:42:31 +0100

    update

--- Teste.R
+++ Teste.R
@@ -1,1 +1,9 @@
-Teste12
+library(blogdown)
+install_hugo()
+hugo_version()
+
+blogdown::new_site(theme = "MarcusVirg/forty", 
+                     sample = TRUE, 
+                     theme_example = TRUE, 
+                     empty_dirs = TRUE, 
+                     to_yaml = TRUE)
